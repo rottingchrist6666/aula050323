6db0ac2eb602716a7743f1388742f4698910b7c5
rottingchrist6666 <joviolcunha@gmail.com>
2023-04-05 20:13:43 -0400

    Aula 05/03

---
+++ app/src/main/java/com/example/aula050323/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.aula050323;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int gray = Color.GRAY;
+    }
+}
