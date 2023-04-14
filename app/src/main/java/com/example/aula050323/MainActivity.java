a6cca07728e989f027bd5673aad13147937d4f2f
rottingchrist6666 <joviolcunha@gmail.com>
2023-04-14 12:26:19 -0400

    mudanças feitas em casa

--- app/src/main/java/com/example/aula050323/MainActivity.java
+++ app/src/main/java/com/example/aula050323/MainActivity.java
@@ -2,9 +2,14 @@ package com.example.aula050323;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +20,19 @@ public class MainActivity extends AppCompatActivity {
         int gray = Color.GRAY;
         TextView gotoregister;
         gotoregister = findViewById(R.id.gotoregister);
+        MaterialButton login = findViewById(R.id.botao);
+        EditText user, senha;
+        user = findViewById(R.id.userbox);
+        senha = findViewById(R.id.passwordbox);
+        String userstring, senhastring;
+        gotoregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),register.class);
+                startActivity(intent);
+                finish();
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/aula050323/register.java
+++ app/src/main/java/com/example/aula050323/register.java
@@ -2,6 +2,7 @@ package com.example.aula050323;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -24,7 +25,14 @@ public class register extends AppCompatActivity {
         checkPass = findViewById(R.id.checkpassword);
         rbutton = findViewById(R.id.botao);
         gotologin = findViewById(R.id.backtolog);
-
+        gotologin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent2 = new Intent(getApplicationContext(),MainActivity.class);
+                      startActivity(intent2);
+                      finish();
+            }
+        });
         rbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -32,7 +40,13 @@ public class register extends AppCompatActivity {
                 email = txtEmail.getText().toString();
                 password = String.valueOf(txtPass.getText());
                 if(TextUtils.isEmpty(email)){
-                    Toast.makeText(this, "no user", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(register.this,"no user",Toast.LENGTH_SHORT).show();
+                }
+                else if(TextUtils.isEmpty(password)){
+                    Toast.makeText(register.this,"no password",Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(register.this,"usuario registrado",Toast.LENGTH_SHORT).show();
                 }
             }
         });
